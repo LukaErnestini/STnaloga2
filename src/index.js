74cd5c2eb2cf8122d9b921f192793ebbb67f196d
LukaErnestini <luka.ernestini@gmail.com>
2021-07-25 15:24:21 +0200

    signup, login work

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 require('dotenv').config();
 require('./db/mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const hbs = require('hbs');
+
 const app = express();
 
-//Models
-
-// Routers
-
-// Define paths for express config
 const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup static directory to serve
 app.use(express.static(publicDirPath));
 
 // view engine configuration
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../templates/views'));
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Middleware use
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+// create application/json parser
+const jsonParser = bodyParser.json();
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(jsonParser);
+app.use(urlencodedParser);
 
-app.listen(3000, () => console.log('Server Up and running'));
+// Routers
+const userRouter = require('./routers/user');
+app.use(userRouter);
+
+app.listen(process.env.PORT, () => console.log('Server up and running'));
---
+++ src/models/user.js
@@ -0,0 +1,102 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+// Triggers before deleting user
+userSchema.pre('remove', async function (next) {
+  const user = this;
+  next();
+});
+
+userSchema.statics.findByCredentials = async (username, password) => {
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    throw new Error('Crikey! Unable to log in.');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Crikey! Unable to log in.');
+  }
+
+  return user;
+};
+
+// Instance method
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    { _id: user._id.toString() },
+    process.env.JWT_TOKEN_KEY,
+    {
+      expiresIn: '7 days',
+    }
+  );
+
+  user.tokens = user.tokens.concat({ token });
+
+  await user.save();
+
+  return token;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
